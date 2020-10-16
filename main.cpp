0f22ad1c7b415e1dc26d5b41d74cbd8d47583e36
juan pablo gomez <pablo.gomez2@idea.edu.co>
2020-10-16 15:52:33 +0200

    Encuentra los digitos de un numero ingresado

--- main.cpp
+++ main.cpp
@@ -4,11 +4,19 @@ using namespace std;
 
 int main()
 {
-    int num[]={1,2,3,4,5};
+    int numero,contador=0;
+    cout<<"ingrese un numero"<<endl;
+    cin>>numero;
+    while (numero>0) {
+        numero=numero/10;
+        contador++;
+    }
+    cout<<"el numero ingresado tiene "<<contador<<" digitos"<<endl;
+    /*int num[]={1,2,3,4,5};
     int *dir_num;
     dir_num=num;
     for(int i=0;i<5;i++){
         cout<<*(dir_num+i)<<endl;
-    }
+    }*/
     return 0;
 }
